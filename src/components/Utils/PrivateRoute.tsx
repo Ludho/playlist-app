fadf8a7dd94ecbb29ade74cbe31de2743257adb4
Ludho <Ludho234@gmail.com>
2022-12-21 01:23:55 +0100

    🐛 fix auth verification

--- src/components/Utils/PrivateRoute.tsx
+++ src/components/Utils/PrivateRoute.tsx
@@ -1,3 +1,6 @@
+import axios from "axios";
+import { useEffect } from "react";
+
 interface IProps {
     element: JSX.Element;
     redirect?: string;
@@ -5,9 +8,19 @@ interface IProps {
 }
 
 const PrivateRoute = ({element,redirect='/',doRedirect=false }: IProps) => {
-    console.log(doRedirect);
-  //  if(doRedirect)window.location.href = redirect;
 
+    useEffect(() => {
+        axios
+        .get(process.env.REACT_APP_API_URL + "/authentification", {
+          withCredentials: true,
+        })
+        .then((user: any) => {
+        }).catch(()=>{
+          //setUser(null);
+          window.location.href = redirect;
+        });
+      }, []);
+      
     return (
         <>
             {element}
